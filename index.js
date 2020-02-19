c72cbe235b548225aa15f357f238316125455564
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-19 02:36:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   var result = []
-  
   for (let i = 0; i < musicians.length; i++){
     result.push( musicians[i] + " plays " + instruments[i])
   }

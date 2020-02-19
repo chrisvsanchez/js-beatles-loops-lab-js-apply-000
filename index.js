203977f3a9831d24192dc1619a46eff7ef0dac86
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-19 02:56:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function iLoveTheBeatles(num){
   let newarr = [];
   let i = 0;
   do{
-    newarr.push("I love the Beatles!" + i);
+    newarr.push("I love the Beatles!");
     i++;
   }
   while (i <= num);

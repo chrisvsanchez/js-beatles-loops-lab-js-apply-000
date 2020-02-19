2bce9693ba943a6856f56215d652169b90ae6a6f
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-19 02:28:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var result = []
+  
+  for (let i = 0; i < musicians.length; i++){
+    result.push( musicians[i] + " plays " + instruments[i])
+  }
+  return result;
+}
+
+function johnLennonPlay(facts){
+  var result =[ ];
+  while (i < facts.length){
+    push.result(facts[i] + "!!!");
+    i++;
+  }
+  return result;
+}
